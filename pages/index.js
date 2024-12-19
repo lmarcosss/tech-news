3113c34f3e74618fb1df7b03f1ca9f86dc015c68
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-12-19 18:54:31 -0300

    Include new text on home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Hello, World!</h1>
+    return <h1>
+        Hello my friend, you are welcome! {" "}
+        <a href="https://www.youtube.com/watch?v=YLy-X0QWuWA">Get your prize.</a>
+    </h1>
 }
 
 export default Home
