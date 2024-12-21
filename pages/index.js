e03886eb86bde9b3b3192b06f570ef01ad0963e4
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2024-12-21 19:32:47 -0300

    include scripts of check and fix lint

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <h1>
-        Hello my friend, you are welcome! {" "}
-        <a href="https://www.youtube.com/watch?v=YLy-X0QWuWA">Get your prize.</a>
+  return (
+    <h1>
+      Hello my friend, you are welcome!{" "}
+      <a href="https://www.youtube.com/watch?v=YLy-X0QWuWA">Get your prize.</a>
     </h1>
+  );
 }
 
-export default Home
+export default Home;
