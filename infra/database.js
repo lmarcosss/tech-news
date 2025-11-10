e13fdc4bee9935110e4a461e3c523a7bac1d709c
Leonardo <marcosleonardosss@gmail.com>
2025-11-10 13:59:14 -0300

    Include migrations get and post

--- infra/database.js
+++ infra/database.js
@@ -4,20 +4,18 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: getSSLValues(),
   });
 
-  
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error)
-
+    console.error(error);
     throw error;
   } finally {
     await client.end();
@@ -27,3 +25,13 @@ async function query(queryObject) {
 export default {
   query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,21 +1,13 @@
-
 test("GET to /api/v1/status returns 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
-  const parsedValue = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBe(parsedValue);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  const databaseInfo = responseBody.dependencies.database;
-
-  expect(databaseInfo.version).toEqual("16.0");
-  expect(databaseInfo.max_connections).toEqual(100);
-  expect(databaseInfo.open_connections).toEqual(1);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
-
-// test("SQL Injection test", async () => {
-//   await fetch(`${process.env.API_URL}/api/v1/status?databaseName=';SELECT pg_sleep(4); --`);
-// });
