81276addbd6dcd10f9d49e3ac9895427dc67a19c
Leonardo dos Santos Marcos <marcosleonardosss@gmail.com>
2025-11-24 14:22:01 -0300

    Include `eslint` on `linting.yaml`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,11 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);
